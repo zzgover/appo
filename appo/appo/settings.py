e4b671d3ad578fd017ac7c3fbf2b36f62f0e2dbd
zzgover <1440688542@qq.com>
2020-05-21 14:39:20 +0800

    尹家琪毕业设计最终版

--- appo/appo/settings.py
+++ appo/appo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=hbez*w6dzi1$38!7!2(+!ow_=g)s1#qes79c3d_lg&rehb(tc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
